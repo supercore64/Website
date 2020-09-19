b6a589f57f0578024da964115c920a3abc25edd7
supercore64 <52065774+supercore64@users.noreply.github.com>
2020-09-19 23:18:57 +1000

    Add files via upload

---
+++ Buttons.js
@@ -0,0 +1,23 @@
+function Button(ID){
+if(ID == 0){
+window.open("Index.html", "_self");
+}
+if(ID == 1){
+window.open("Maths.html", "_self");
+}
+if(ID == 2){
+window.open("English.html", "_self");
+}
+if(ID == 3){
+window.open("Science.html", "_self");
+}
+if(ID == 4){
+window.open("History.html", "_self");
+}
+if(ID == 5){
+window.open("Geography.html", "_self");
+}
+if(ID == 6){
+window.open("Music.html", "_self");
+}
+}
---
+++ Script.js
@@ -0,0 +1,6 @@
+console.log("Script.js Loaded")
+y = 0;
+function Button(ID){
+x = ID + ".html";
+window.open(x, "_self");
+}
